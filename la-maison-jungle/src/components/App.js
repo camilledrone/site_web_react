4c9768e07959e9f5971c1d089adba65c639b7034
Camille <camille.boucher@imt-atlantique.net>
2022-03-15 16:47:58 +0100

    finalisation of the front

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import Cart from './Cart'
 import ShoppingList from './ShoppingList'
@@ -5,12 +6,18 @@ import Footer from './Footer'
 import '../styles/Layout.css'
 
 function App() {
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner />
 			<div className='lmj-layout-inner'>
-				<Cart />
-				<ShoppingList />
+				<Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
 			</div>
 			<Footer />
 		</div>
--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -2,11 +2,13 @@ import { plantList } from '../datas/plantList'
 import CareScale from './CareScale'
 import PlantItem from './PlantItem';
 import '../styles/ShoppingList.css'
+import { useState } from 'react';
+import Categories from './Categories';
 
-function ShoppingList() {
+function ShoppingList({cart, updateCart}) {
+
+    const [activeCategory, setActiveCategory] = useState('')
     // fonction reduce : array1.reduce((previousValue, currentValue) => previousValue + currentValue,initialValue)
-
-
     const categories = plantList.reduce(
         //call back
         /* La méthode concat() est utilisée afin de fusionner deux ou plusieurs tableaux en les concaténant. 
@@ -19,30 +21,49 @@ function ShoppingList() {
         //si non on concatène la nouvelle catégorie
         (current, plant) => current.includes(plant.category) ? current : current.concat(plant.category),
         // initial value
-        []);    
+        []); 
+            
+        function addToCart(name, price) {
+            const currentPlantSaved = cart.find((plant) => plant.name === name)
+            if (currentPlantSaved) {
+                const cartFilteredCurrentPlant = cart.filter(
+                    (plant) => plant.name !== name
+                )
+                updateCart([
+                    ...cartFilteredCurrentPlant,
+                    { name, price, amount: currentPlantSaved.amount + 1 }
+                ])
+            } else {
+                updateCart([...cart, { name, price, amount: 1 }])
+            }
+        }
 
-    return (
-        <div>
-            <ul>
-                {categories.map((category) => (
-                    <li key={category}>{category}</li>
-                ))}
-            </ul>
-            <ul className='lmj-plant-list'>
-				{plantList.map(({id, name, cover, light, water}) => ( 
-                    <PlantItem
-                            id={id}
-                            name = {name}
-                            cover = {cover}
-                            light = {light}
-                            water = {water}
-                    />
-				))}
-			</ul>
-        </div>
+        return (
+            <div className='lmj-shopping-list'>
+                <Categories
+				    categories={categories}
+				    setActiveCategory={setActiveCategory}
+				    activeCategory={activeCategory}
+			    />
 
-        
-    );
+                <ul className='lmj-plant-list'>
+                    {plantList.map(({ id, cover, name, water, light, price,category }) => 
+                        !activeCategory || activeCategory === category ?
+                        (
+					        <div key={id}>
+						        <PlantItem
+							        cover={cover}
+							        name={name}
+							        water={water}
+							        light={light}
+							        price={price}
+						        />
+						        <button onClick={() => addToCart(name, price)}>Ajouter</button>
+					        </div>
+				    ) : null )}
+                </ul>
+            </div>
+        );
 }
 
 export default ShoppingList
